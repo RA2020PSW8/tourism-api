7391cd87779b1a1d5e8103e309e05f19dbef03d8
Mihailo <zupermancile@gmail.com>
2023-10-14 19:44:09 +0200

    feat: started forum comment feature

---
+++ src/Modules/Blog/Explorer.Blog.API/Public/Comment/IForumCommentService.cs
@@ -0,0 +1,18 @@
+﻿using Explorer.Blog.API.Dtos;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.API.Public.Comment
+{
+    public interface IForumCommentService
+    {
+        Result<ForumCommentDto> Get(int id);
+        Result<ForumCommentDto> Create(ForumCommentDto forumCommentDto);
+        Result<ForumCommentDto> Update(ForumCommentDto forumCommentDto);
+        Result Delete(int id);
+    }
+}
