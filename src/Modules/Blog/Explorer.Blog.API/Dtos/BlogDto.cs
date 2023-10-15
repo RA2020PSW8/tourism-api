a11561e0e306c7761ef7c3da0a544856a37b8776
Ognjen <ognjenmilojevic2001@gmail.com>
2023-10-15 13:09:30 +0200

    feat: implemented crud endpoints for blog entity

---
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.API.Dtos
+{
+    public class BlogDto
+    {
+        public enum BlogStatus { DRAFT, PUBLISHED, CLOSED }
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public List<string>? ImageLinks { get; set; }
+        public BlogStatus Status { get; set; }
+    }
+}
