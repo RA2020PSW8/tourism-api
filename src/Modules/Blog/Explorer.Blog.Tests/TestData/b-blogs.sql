f8f794ef8557b2d0371b62c9ed69331a8775bff3
Ognjen <ognjenmilojevic2001@gmail.com>
2023-10-15 21:52:36 +0200

    test: implemented xuint tests for blog

---
+++ src/Modules/Blog/Explorer.Blog.Tests/TestData/b-blogs.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO blog."Blogs"("Id","Title","Description","CreationDate","Status") 
+VALUES(-1,'Wonderful visit to Serbia', 'It was wonderful', '01-01-2023',0);
+
+INSERT INTO blog."Blogs"("Id","Title","Description","CreationDate","Status") 
+VALUES(-2,'Terrific visit to Spain', 'It was terrific', '02-02-2023',0);
+
+INSERT INTO blog."Blogs"("Id","Title","Description","CreationDate","Status") 
+VALUES(-3,'Supreme visit to Italy', 'It was supreme', '03-03-2023',0);
+
+
