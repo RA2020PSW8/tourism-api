371080bb9d02ad69217368876eabc36c89f13360
Strahinja <strahinja.praska6@gmail.com>
2023-10-16 23:53:03 +0100

    feat: club crud

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Club.cs
@@ -0,0 +1,30 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.Core.Domain
+{
+  
+
+    public class Club: Entity 
+    {
+        public string Name { get; init; }
+        public string ?Description { get; init; }
+
+        public string ?Image { get; init; }
+
+        public Club(string name, string? description, string? image)
+        {
+
+            if(string.IsNullOrEmpty(name)) throw new ArgumentNullException("Invalid name.");
+            Name = name; 
+            Description = description;
+            Image = image;  
+        }
+
+
+    }
+}
