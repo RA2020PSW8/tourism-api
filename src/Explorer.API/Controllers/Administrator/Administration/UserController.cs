ea6c4a2358f6d7bed33862e6f1b8bf00b1cfff93
Anja Kovacevic <anjakovacevic9455@gmail.com>
2023-10-16 18:35:04 +0200

    implement user viewing

---
+++ src/Explorer.API/Controllers/Administrator/Administration/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Administrator.Administration
+{
+    [Authorize(Policy = "administratorPolicy")]
+    [Route("api/administration/users")]
+    public class UserController : BaseApiController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<UserDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _userService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        //[HttpPost]
+        //public ActionResult<EquipmentDto> Create([FromBody] EquipmentDto equipment)
+        //{
+        //    var result = _equipmentService.Create(equipment);
+        //    return CreateResponse(result);
+        //}
+
+        //[HttpPut("{id:int}")]
+        //public ActionResult<EquipmentDto> Update([FromBody] EquipmentDto equipment)
+        //{
+        //    var result = _equipmentService.Update(equipment);
+        //    return CreateResponse(result);
+        //}
+
+        //[HttpDelete("{id:int}")]
+        //public ActionResult Delete(int id)
+        //{
+        //    var result = _equipmentService.Delete(id);
+        //    return CreateResponse(result);
+        //}
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/UserDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Stakeholders.API.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+        public bool IsActive { get; set; }
+    }
+    public enum UserRole
+    {
+        Administrator,
+        Author,
+        Tourist
+    }
+
+}
