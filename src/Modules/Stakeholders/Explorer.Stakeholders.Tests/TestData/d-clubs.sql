b5bb1b61d54dca99123a56ddb7fca62f6ec5378d
Strahinja <strahinja.praska6@gmail.com>
2023-10-17 00:52:29 +0100

    test: crud club

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-clubs.sql
@@ -0,0 +1,8 @@
+INSERT INTO stakeholders."Clubs"("Id", "Name", "Description", "Image")
+VALUES (-1, 'Klub 1', 'Dobar', 'klub1.png');
+
+INSERT INTO stakeholders."Clubs"("Id", "Name", "Description", "Image")
+VALUES (-2, 'Klub 2', 'Dobar', 'klub2.png');
+
+INSERT INTO stakeholders."Clubs"("Id", "Name", "Description", "Image")
+VALUES (-3, 'Klub 3', 'Dobar', 'klub3.png');
