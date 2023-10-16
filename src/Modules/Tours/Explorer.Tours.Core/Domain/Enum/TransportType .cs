fc41edfff04503a2865edc58dd90cfaab4a615e8
Pisaric <116560852+Pisaric@users.noreply.github.com>
2023-10-16 21:50:54 +0200

    added Tour in Domain

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Enum/TransportType .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain.Enum
+{
+    public enum TransportType
+    {
+        WALK,
+        BIKE,
+        CAR,
+        BOAT
+    }
+
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Tour.cs
@@ -0,0 +1,48 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using Explorer.Tours.Core.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class Tour : Entity
+    {
+        public long UserId { get; init; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public double Price { get; init; }
+        public TourDifficulty? Difficulty { get; init; }    
+        public TransportType? TransportType { get; init; }
+        public TourStatus? Status { get; init; }
+        public List<String> Tags { get; init; }
+
+        public Tour()
+        {
+            Tags = new List<String>();
+        }
+
+        public Tour(long userId, string name, string description, double price, TourDifficulty? difficulty, TransportType? transportType, TourStatus? status, List<string> tags)
+        {
+            UserId = userId;
+            Name = name;
+            Description = description;
+            Price = price;
+            Difficulty = difficulty;
+            TransportType = transportType;
+            Status = status;
+            Tags = tags;
+
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (UserId <= 0) throw new ArgumentException("Invalid UserId");
+            if (string.IsNullOrWhiteSpace(Description)) throw new ArgumentException("Invalid description.");
+            if (Price < 0) throw new Exception("Invalid price.");
+        }
+    }
+}
