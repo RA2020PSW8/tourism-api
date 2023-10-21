d4ad97c133d07dc23db4c97b2d05ac214141fc1a
Ognjen Milojević <70882711+ognjenm01@users.noreply.github.com>
2023-10-21 12:17:01 +0200

    Feat/blog (#7)
    * feat: implemented crud endpoints for blog entity
    
    * test: implemented xuint tests for blog
    
    * fix: temporary changed ImageLinks from string[] to string
    
    * fix: fixed broken sql file

---
+++ src/Modules/Blog/Explorer.Blog.Core/UseCases/Blog/BlogService.cs
@@ -0,0 +1,22 @@
+﻿using Explorer.Blog.API.Dtos;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.Blog.Core.Domain;
+using AutoMapper;
+using Explorer.Blog.API.Public.Blog;
+
+namespace Explorer.Blog.Core.UseCases.Blog
+{
+    public class BlogService : CrudService<BlogDto, Domain.Blog>, IBlogService
+    {
+        public BlogService(ICrudRepository<Domain.Blog> crudRepository, IMapper mapper) : base(crudRepository, mapper)
+        {
+
+        }
+    }
+}
