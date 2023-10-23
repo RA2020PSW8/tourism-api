78c8fb8b64e1a10c9d3b9262db7ffc48c709b8c3
Anja Kovacevic <anjakovacevic9455@gmail.com>
2023-10-23 19:43:31 +0200

    fix: change userRole type

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/UserDto.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/UserDto.cs
@@ -12,16 +12,11 @@ namespace Explorer.Stakeholders.API.Dtos
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public UserRole Role { get; set; }
+        public int Role { get; set; }
         public bool IsActive { get; set; }
         public string Email {  get; set; }
         public bool IsBlocked {  get; set; }
     }
-    public enum UserRole
-    {
-        Administrator,
-        Author,
-        Tourist
-    }
+   
 
 }
