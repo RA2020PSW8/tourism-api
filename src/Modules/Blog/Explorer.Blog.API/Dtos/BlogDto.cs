3a5f560ea8dde28148cdc99473d2a8b9ebf6b2b4
Ognjen <ognjenmilojevic2001@gmail.com>
2023-10-28 19:09:51 +0200

    fix: expanded blog model with new fields

--- src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Explorer.Blog.API.Dtos.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,12 @@ namespace Explorer.Blog.API.Dtos
 {
     public class BlogDto
     {
-        public enum BlogStatus { DRAFT, PUBLISHED, CLOSED }
         public int Id { get; set; }
+        public int CreatorId { get; set; }
         public required string Title { get; set; }
         public required string Description { get; set; }
-        public DateTime CreationDate { get; set; }
-        //public List<string>? ImageLinks { get; set; }
-        public string? ImageLinks { get; set; }
         public BlogStatus Status { get; set; }
+        public DateOnly CreationDate { get; set; }
+        public List<string>? ImageLinks { get; set; }
     }
 }
--- src/Modules/Blog/Explorer.Blog.Tests/TestData/b-blogs.sql
+++ src/Modules/Blog/Explorer.Blog.Tests/TestData/b-blogs.sql
@@ -1,10 +1,9 @@
-﻿INSERT INTO blog."Blogs"("Id","Title","Description","CreationDate","Status","ImageLinks") 
-VALUES(-1,'Wonderful visit to Serbia', 'It was wonderful', '01-01-2023',0,'img1.jpg');
+﻿INSERT INTO blog."Blogs"("Id", "CreatorId", "Title", "Description", "Status", "CreationDate", "ImageLinks") 
+VALUES(-1, -1, 'Wonderful visit to Serbia', 'It was wonderful', 0, '01-01-2023', ARRAY[('img1.jpg', 'img2.jpg')]);
 
-INSERT INTO blog."Blogs"("Id","Title","Description","CreationDate","Status", "ImageLinks") 
-VALUES(-2,'Terrific visit to Spain', 'It was terrific', '02-02-2023',0,'img2.jpg');
+INSERT INTO blog."Blogs"("Id", "CreatorId", "Title", "Description", "Status", "CreationDate", "ImageLinks") 
+VALUES(-2, -2, 'Terrific visit to Spain', 'It was terrific', 0, '02-02-2023', ARRAY[('img3.jpg', 'img4.jpg')]);
 
-INSERT INTO blog."Blogs"("Id","Title","Description","CreationDate","Status", "ImageLinks") 
-VALUES(-3,'Supreme visit to Italy', 'It was supreme', '03-03-2023',0,'img3.jpg');
-
+INSERT INTO blog."Blogs"("Id", "CreatorId", "Title", "Description", "Status", "CreationDate", "ImageLinks") 
+VALUES(-3, -3, 'Supreme visit to Italy', 'It was supreme', 0, '03-03-2023', ARRAY[('img5.jpg', 'img6.jpg')]);
 
