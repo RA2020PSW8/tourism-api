d3d32ce5964425dcde9e830b4346f69fb187bf76
Milica Kljajić <116972806+miilicakljajic@users.noreply.github.com>
2023-11-01 16:26:36 +0100

    fix: moved tour issue from Tours module to Stakeholders module (#29)
    Co-authored-by: Ognjen Milojević <70882711+ognjenm01@users.noreply.github.com>

--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -11,7 +11,6 @@ public class ToursProfile : Profile
         CreateMap<EquipmentDto, Equipment>().ReverseMap();
         CreateMap<TouristEquipmentDto, TouristEquipment>().ReverseMap();
         CreateMap<TourReviewDto, TourReview>().ReverseMap();
-        CreateMap<TourIssueDto, TourIssue>().ReverseMap();
         CreateMap<KeypointDto, Keypoint>().ReverseMap();
         CreateMap<ObjectDto, Domain.Object>().ReverseMap();
         CreateMap<TourPreferenceDto, TourPreference>().ReverseMap();
--- src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
@@ -3,7 +3,6 @@ DELETE FROM tours."Objects";
 DELETE FROM tours."Equipment";
 DELETE FROM tours."TourReviews";
 DELETE FROM tours."TourPreference";
-DELETE FROM tours."TourIssue";
 DELETE FROM tours."Keypoints";
 DELETE FROM tours."TouristEquipment";
 DELETE FROM tours."Tours";
