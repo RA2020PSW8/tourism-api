081976758ac2f9a25b1c3f85abc102b20ae1f2df
jomax01 <116906075+jomax01@users.noreply.github.com>
2023-11-10 15:43:21 +0100

    feat: tour filtering by coordinates (#36)
    * feat : filtered tours by keypoint criteria
    
    * Squashed commit of the following:
    
    commit ef603b2445d5c74fb5083212d9c00db0149d99bb
    Author: Ognjen Milojević <70882711+ognjenm01@users.noreply.github.com>
    Date:   Fri Nov 3 17:21:51 2023 +0100
    
        fix: blog management (#32)
    
        * fix: expanded blog model with new fields
    
        * feat: auto load creator data & create admin protection
    
        * fix: updated tests to support blog agregation
    
        * fix: added mapper for person and minor dto change
    
        * fix: fixed tests
    
    * debug : tourFiltering
    
    * chore: fixed output for PagedResult
    
    * chore: refactorisation
    
    * chore : rafactorisation
    
    * refactor & fix: remove spaces & add paged logic that returns all tours
    
    * fix: pagination
    
    * chore :added test
    
    ---------
    
    Co-authored-by: JelenaVujovic <jelenaena.3a@gmail.com>

--- src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
@@ -43,6 +43,7 @@ public static class ToursStartup
         services.AddScoped<IObjectService, ObjectService>();
         services.AddScoped<ITourEquipmentService, TourEquipmentService>();
         services.AddScoped<ITouristPositionService, TouristPositionService>();
+        services.AddScoped<ITourFilteringService, TourFilteringService>();
         services.AddScoped<IOrderItemService, OrderItemService>();
         services.AddScoped<IShoppingCartService, ShoppingCartService>();
     }
