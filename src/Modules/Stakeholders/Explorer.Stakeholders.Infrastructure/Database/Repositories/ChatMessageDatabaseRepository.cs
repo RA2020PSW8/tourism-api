a3bdde4d2f9e5eed646f203dd460df832a0941a4
Nikola Simic <nikola7simic@gmail.com>
2023-11-11 11:31:35 +0100

    feat: add repository method that returns last message of every follower messaged (not tested)

--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/ChatMessageDatabaseRepository.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/ChatMessageDatabaseRepository.cs
@@ -9,15 +9,15 @@ namespace Explorer.Stakeholders.Infrastructure.Database.Repositories
     public class ChatMessageDatabaseRepository: CrudDatabaseRepository<ChatMessage, StakeholdersContext>, IChatMessageRepository
     {
 
-        private readonly StakeholdersContext _dbContext;
+        private readonly DbSet<ChatMessage> _dbSet;
 
         public ChatMessageDatabaseRepository(StakeholdersContext dbContext) : base(dbContext)
         {
-            _dbContext = dbContext;
+            _dbSet = dbContext.Set<ChatMessage>();
         }
         public PagedResult<ChatMessage> GetConversation(long firstParticipantId, long secondParticipantId)
         {
-            var chatMessages = _dbContext.ChatMessages
+            var chatMessages = _dbSet
                 .AsNoTracking()
                 .Where(cm => (cm.SenderId == firstParticipantId && cm.ReceiverId == secondParticipantId) ||
                              (cm.SenderId == secondParticipantId && cm.ReceiverId == firstParticipantId))
@@ -27,6 +27,36 @@ namespace Explorer.Stakeholders.Infrastructure.Database.Repositories
                 .ToList();
 
             return new PagedResult<ChatMessage>(chatMessages, chatMessages.Count);
+        }
+
+        public IEnumerable<ChatMessage> GetMessagesForPreview(long userId)
+        {
+            var chatMessages = _dbSet
+                .AsNoTracking()
+                .DistinctBy(c => new { c.SenderId, c.ReceiverId })
+                .Where(c => c.SenderId == userId || c.ReceiverId == userId)
+                .OrderBy(c => new { c.SenderId, c.ReceiverId, c.CreationDateTime });
+
+            return RemovePreviewDuplicates(chatMessages);
+        }
+
+        private IEnumerable<ChatMessage> RemovePreviewDuplicates(IEnumerable<ChatMessage> messages)
+        {
+            List<ChatMessage> reducedMessages = new List<ChatMessage>();
+            foreach (var chatm in messages)
+            {
+                if (reducedMessages.FirstOrDefault(m => m.SenderId == chatm.ReceiverId || m.ReceiverId == chatm.SenderId) != null)
+                    continue;
+                
+                reducedMessages.Add(
+                    messages
+                        .Where(m => m.SenderId == chatm.ReceiverId || m.ReceiverId == chatm.SenderId)
+                        .OrderBy(m => m.CreationDateTime)
+                        .FirstOrDefault()
+                    );
+            }
+
+            return reducedMessages;
         }
     }
 }
