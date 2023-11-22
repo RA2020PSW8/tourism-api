9f424538df320feca9948d2f8d02729d9ead960f
srdjanpetronijevic <srdjanpetronijevic@outlook.com>
2023-11-22 22:03:14 +0100

    fix: change tours to payments schema in insert scripts (+10 teletabis poena)

--- src/Modules/Payments/Explorer.Payments.Tests/TestData/h-shoppingCart.sql
+++ src/Modules/Payments/Explorer.Payments.Tests/TestData/h-shoppingCart.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO tours."ShoppingCarts"("Id", "UserId", "OrdersId", "Price")
+﻿INSERT INTO payments."ShoppingCarts"("Id", "UserId", "OrdersId", "Price")
 VALUES (-1, -1, ARRAY[(-1)], 150.0);
-INSERT INTO tours."ShoppingCarts"("Id", "UserId", "OrdersId", "Price")
+INSERT INTO payments."ShoppingCarts"("Id", "UserId", "OrdersId", "Price")
 VALUES (-2, -1, ARRAY[(-1)], 150.0);
--- src/Modules/Payments/Explorer.Payments.Tests/TestData/j-tourPurchaseToken.sql
+++ src/Modules/Payments/Explorer.Payments.Tests/TestData/j-tourPurchaseToken.sql
@@ -1,8 +1,8 @@
-﻿INSERT INTO tours."TourPurchaseTokens"("Id", "TourId", "TouristId")
+﻿INSERT INTO payments."TourPurchaseTokens"("Id", "TourId", "TouristId")
 VALUES (-1, -1, 1);
 
-INSERT INTO tours."TourPurchaseTokens"("Id", "TourId", "TouristId")
+INSERT INTO payments."TourPurchaseTokens"("Id", "TourId", "TouristId")
 VALUES (-2, -2, 1);
 
-INSERT INTO tours."TourPurchaseTokens"("Id", "TourId", "TouristId")
+INSERT INTO payments."TourPurchaseTokens"("Id", "TourId", "TouristId")
 VALUES (-3, -3, 2);
