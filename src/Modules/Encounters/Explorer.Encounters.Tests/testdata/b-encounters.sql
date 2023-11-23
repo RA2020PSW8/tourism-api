01d8ccbcc1227c8f1e0345751a5b31e1ffe59504
milena <milenamarkovickg@gmail.com>
2023-11-23 11:04:50 +0100

    test: add tests for encounters crud operations

---
+++ src/Modules/Encounters/Explorer.Encounters.Tests/testdata/b-encounters.sql
@@ -0,0 +1,11 @@
+INSERT INTO encounters."Encounters"(
+	"Id", "UserId", "Name", "Description", "Latitude", "Longitude", "Xp", "Status", "Type")
+	VALUES (-1, 1, 'Encounter1', 'Description1', 23.34, 23.34, 4, 0, 0);
+
+INSERT INTO encounters."Encounters"(
+	"Id", "UserId", "Name", "Description", "Latitude", "Longitude", "Xp", "Status", "Type")
+	VALUES (-2, 1, 'Encounter2', 'Description3', 23.34, 56.34, 6, 0, 0);
+
+INSERT INTO encounters."Encounters"(
+	"Id", "UserId", "Name", "Description", "Latitude", "Longitude", "Xp", "Status", "Type")
+	VALUES (-3, 1, 'Encounter2', 'Description3', 23.34, 32.34, 2, 0, 0);
