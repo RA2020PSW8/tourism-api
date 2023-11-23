255a5f30df6f550566728a6a64a9b96bdd5f5bb0
Anastasija Radic <116906142+anastasijaradic@users.noreply.github.com>
2023-11-23 21:20:34 +0100

    fix: Add some data to tourPurchaseToken

--- src/Modules/Payments/Explorer.Payments.Tests/TestData/j-tourPurchaseToken.sql
+++ src/Modules/Payments/Explorer.Payments.Tests/TestData/j-tourPurchaseToken.sql
@@ -6,3 +6,6 @@ VALUES (-2, -2, 1);
 
 INSERT INTO payments."TourPurchaseTokens"("Id", "TourId", "TouristId")
 VALUES (-3, -3, 2);
+
+INSERT INTO payments."TourPurchaseTokens"("Id", "TourId", "TouristId")
+VALUES (-4, -1, -1);
