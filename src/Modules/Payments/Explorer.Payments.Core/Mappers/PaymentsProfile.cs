3b9b2182a6f2d21d8ea4a207467d6d0660506bf6
jomax01 <116906075+jomax01@users.noreply.github.com>
2023-12-26 19:50:16 +0100

    feat: tour bundle (#74)
    * feat : CRUD operations for bundle
    
    * feat: GetById
    
    * feat: solution for join problem
    
    Problem was joining tables between different modules.
    NOTE: this many files changed because of moving files and renaming
    
    * feat: bundle-publishing
    
    * feat: bundle publishing and archiving
    
    * fix: authorization policy
    
    * feat : tests implemented and calcualting price

--- src/Modules/Payments/Explorer.Payments.Core/Mappers/PaymentsProfile.cs
+++ src/Modules/Payments/Explorer.Payments.Core/Mappers/PaymentsProfile.cs
@@ -11,7 +11,9 @@ public class PaymentsProfile : Profile
         CreateMap<OrderItemDto, OrderItem>().ReverseMap();
         CreateMap<ShoppingCartDto, ShoppingCart>().ReverseMap();
         CreateMap<TourPurchaseTokenDto, TourPurchaseToken>().ReverseMap();
+        CreateMap<BundlePriceDto, BundlePrice>().ReverseMap();
         CreateMap<WalletDto, Wallet>().ReverseMap();
         CreateMap<PaymentRecordDto, PaymentRecord>().ReverseMap();
+
     }
 }
