fb50226a38cbdbb9a10776d045cb1322f4c7320e
MiljanaMa <miljana.marjanovic9@gmail.com>
2023-12-26 17:30:06 +0100

    bugfix: enum in achievement dto

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/AchievementDto.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/AchievementDto.cs
@@ -1,11 +1,13 @@
 ﻿
 
+using Explorer.Stakeholders.API.Dtos.Enums;
+
 namespace Explorer.Stakeholders.API.Dtos
 {
     public class AchievementDto
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public int Type { get; set; }
+        public AchievementType Type { get; set; }
     }
 }
